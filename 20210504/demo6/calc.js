759c1662528048670dbdef08076e8b1cef7ffca6
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-04 19:47:35 -0600

    Nuevas demotraciones

---
+++ 20210504/demo6/calc.js
@@ -0,0 +1,20 @@
+class Calc {
+    sum(a,b)
+    {
+        return a + b;
+    }
+    substract(a,b)
+    {
+        return a - b;
+    }
+    multiply(a,b)
+    {
+        return a * b;
+    }
+    division(a,b)
+    {
+        return a / b;
+    }
+}
+
+let result = 0;
